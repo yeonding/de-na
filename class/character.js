9d4b05d71abfeea7606f0613b66981a2ca34764a
yeonding <yeonding2@naver.com>
2023-03-28 19:17:51 +0900

    [추가|0328] 오후 시간 수정본

---
+++ class/character.js
@@ -0,0 +1,16 @@
+
+class charcter {
+    construcor(){
+        this.hp = hp || 100
+        this.maxHp = maxHp || 100
+        this.exp = exp || 0
+        this.maxExp = maxExp || 100
+        this.movespeed = movespeed || 1
+        this.demage = this.demage || 5
+        this.cooltime = 2//무기 쿨타임
+        this.amount = 3 // 무기 생성 갯수
+    }
+}
+
+const ch1 = charcter;
+
--- menu/select-character.js
+++ menu/select-character.js
@@ -1,24 +1,14 @@
-/**@type {HTMLCanvasElement} */
-
-const canvas = document.querySelector('canvas')
-const c = canvas.getContext('2d')
-
-canvas.width = 1024;
-canvas.height = 576;
 
 const ch1 = document.getElementById('ch1');
 const 캐릭터2 = document.getElementById('ch2');
 const 캐릭터3 = document.getElementById('ch3');
 
-function draw(){
-    c.fillStyle = 'blue'
-    c.fillRect (10,10,50,50)
-}
+
 
 ch1.addEventListener('click', function() {
   // 캐릭터1을 선택했을 때 할 일
   //macincharcter = ch1
-  draw();
+  alert('캐릭터1을 선택했습니다.');
 });
 
 ch2.addEventListener('click', function() {
