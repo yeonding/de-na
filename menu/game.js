055428e8dbf8145d617779c5aad65bc982b7f1d8
yeonding <yeonding2@naver.com>
2023-03-28 20:37:24 +0900

    [추가|0328] 캐릭터선택 후 버튼 클릭 시 선택창 꺼짐 구현

--- menu/game.js
+++ menu/game.js
@@ -1,33 +1,94 @@
 /**@type {HTMLCanvasElement} */
 
-const canvas = document.querySelector('canvas')
-const c = canvas.getContext('2d')
+// 자바스크립트 코드
+const canvas = document.getElementById('game1');
+const ctx = canvas.getContext('2d');
+
 
 canvas.width = 1024;
 canvas.height = 576;
 
-const ch1 = document.getElementById('ch1');
-const 캐릭터2 = document.getElementById('ch2');
-const 캐릭터3 = document.getElementById('ch3');
+ctx.fillStyle = 'blue'
+ctx.fillRect (0, 0, canvas.width, canvas.height)
 
-function draw(){
-    c.fillStyle = 'blue'
-    c.fillRect (10,10,50,50)
-}
+const ch1 = document.getElementById('ch1');
+const ch2 = document.getElementById('ch2');
+const ch3 = document.getElementById('ch3');
+const ch4 = document.getElementById('ch4');
+const ch5 = document.getElementById('ch5');
+
+const startButton = document.getElementById('start-button');
+startButton.style.display = 'none'; // 시작 버튼은 처음에 보이지 않음
+
+let selectedCharacter = null;
+
+//게임시작
+const characterSeletion = document.getElementById('characterSelection');
+function startGame() {
+  //캐릭터 선택 화면 숨기기
+     characterSeletion.classList.add("hidden");
+
+    //
+ }
 
 ch1.addEventListener('click', function() {
-  // 캐릭터1을 선택했을 때 할 일
-  //macincharcter = ch1
-  draw();
+  // 캐릭터1을 선택했을 때
+  console.log('캐릭터1을 선택했습니다.');
+  selectedCharacter = ch1;
+  startButton.style.display = 'block';
 });
 
 ch2.addEventListener('click', function() {
-  // 캐릭터2을 선택했을 때 할 일
+  // 캐릭터2을 선택했을 때
   console.log('캐릭터2을 선택했습니다.');
+  selectedCharacter = ch2;
+  startButton.style.display = 'block';
 });
 
 ch3.addEventListener('click', function() {
-  // 캐릭터3을 선택했을 때 할 일
+  // 캐릭터3을 선택했을 때
   console.log('캐릭터3을 선택했습니다.');
+  selectedCharacter = ch3;
+  startButton.style.display = 'block';
 });
+
+ch4.addEventListener('click', function() {
+    // 캐릭터4를 선택했을 때
+    console.log('캐릭터4을 선택했습니다.');
+    selectedCharacter = ch4;
+    startButton.style.display = 'block';
+  });
+
+ch5.addEventListener('click', function() {
+    // 캐릭터5를 선택했을 때
+    console.log('캐릭터5을 선택했습니다.');
+    selectedCharacter = ch5;
+    startButton.style.display = 'block';
+  });  
+
+startButton.addEventListener('click', function() {
+  // 게임 시작 버튼을 눌렀을 때
+  startGame();
+  console.log('게임을 시작합니다.');
+  // 게임 시작에 필요한 초기화 작업 수행
+  
+  // 선택한 캐릭터 그리기
+  ctx.drawImage(selectedCharacter, 0, 0);
+  
+  // 게임 루프 실행
+  gameLoop();
+});
+
+function gameLoop() {
+  // 게임 루프
+  // 게임의 상태 업데이트
+  
+  // 게임 화면 그리기
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  // 캐릭터 등 게임 요소 그리기
+  
+  // 다음 프레임에 대한 처리를 위해 루프 재귀 호출
+  requestAnimationFrame(gameLoop);
+}
+
 
--- menu/select-character.js
+++ menu/select-character.js
@@ -1,23 +1,81 @@
 
-const ch1 = document.getElementById('ch1');
-const 캐릭터2 = document.getElementById('ch2');
-const 캐릭터3 = document.getElementById('ch3');
+//const characterSelectCanvas = document.getElementById("characterSelectCanvas");
+//const gameCanvas = document.getElementById("gameCanvas");
+//const canvasContainer = document.getElementById("canvasContainer");
 
+const characterSelectCtx = characterSelectCanvas.getContext("2d");
+//const gameCtx = gameCanvas.getContext("2d");
 
+// 캐릭터 이미지 배열
+const characters = [
+  {name: 'Character1', src: "../images/mob/ChargeAttackPreview.gif"},
+  {name: 'Character2', src: "../images/mob/ChargeAttackPreview.gif"},
+  {name: 'Character3', src: "../images/mob/ChargeAttackPreview.gif"}
+  ]
 
-ch1.addEventListener('click', function() {
-  // 캐릭터1을 선택했을 때 할 일
-  //macincharcter = ch1
-  alert('캐릭터1을 선택했습니다.');
+// 캐릭터 이미지 그리기
+function drawCharacters() {
+  let x = 50;
+  let y = 50;
+  characters.forEach(character => {
+    let img = new Image();
+    img.src = character.src;
+    img.onload = function() {
+      characterSelectCtx.drawImage(img, x, y, 100, 100);
+    };
+    x += 150;
+  });
+}
+
+// 선택한 캐릭터 그리기
+function drawSelectedCharacter(character) {
+  characterSelectCtx.clearRect(0, 0, characterSelectCanvas.width, characterSelectCanvas.height);
+  let img = new Image();
+  img.src = character.src;
+  img.onload = function() {
+    characterSelectCtx.drawImage(img, 50, 50, 100, 100);
+  };
+}
+
+// 선택한 캐릭터 정보 저장
+let selectedCharacter;
+
+// 캐릭터 선택 클릭 이벤트
+characterSelectCanvas.addEventListener("click", function(e) {
+  let x = e.offsetX;
+  let y = e.offsetY;
+  characters.forEach(character => {
+    if (x >= 50 && x <= 150 && y >= 50 && y <= 150 && character === characters[0]) {
+      selectedCharacter = character;
+      drawSelectedCharacter(character);
+    }
+    else if (x >= 200 && x <= 300 && y >= 50 && y <= 150 && character === characters[1]) {
+      selectedCharacter = character;
+      drawSelectedCharacter(character);
+    }
+    else if (x >= 350 && x <= 450 && y >= 50 && y <= 150 && character === characters[2]) {
+      selectedCharacter = character;
+      drawSelectedCharacter(character);
+    }
+  });
+  if (selectedCharacter) {
+    characterSelectCanvas.style.display = "none";
+    gameCanvas.style.display = "block";
+    startGame();
+  }
 });
 
-ch2.addEventListener('click', function() {
-  // 캐릭터2을 선택했을 때 할 일
-  console.log('캐릭터2을 선택했습니다.');
-});
+// 게임 시작
+// function startGame() {
+//   // 게임 화면 그리기
+//   gameCtx.fillStyle = "black";
+//   gameCtx.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
+  
+//   // 선택한 캐릭터 이미지 그리기
+//   let img = new Image();
+//   img.src = selectedCharacter.src;
+//   img.onload = function() {
+//     gameCtx.drawImage(img,
 
-ch3.addEventListener('click', function() {
-  // 캐릭터3을 선택했을 때 할 일
-  console.log('캐릭터3을 선택했습니다.');
-});
+
 
