15d86eec41fb950760d9839b6a835e97e5f4e3dd
dnjseo <dnjs.seo@gmail.com>
2023-03-29 15:27:48 +0900

    [추가|0329] stage commit 추가

--- stage/stage-city.js
+++ stage/stage-city.js
@@ -1,168 +1,85 @@
+// 캔버스 세팅
+let canvas;
+let ctx;
+canvas = document.createElement('canvas')
+ctx = canvas.getContext('2d')
+canvas.width = 1024;
+canvas.height = 576;
+document.body.appendChild(canvas)
++8
+// 배경, 플레이어 로딩
+let backgroundImage,playerImage,monsterImage
 
-// class Enemy {
-//     constructor({ position = { x: 0, y: 0 } }) {
-//         this.position = position;
-//         this.width = 100;
-//         this.height = 100;
-//     }
-
-//     draw() {
-//         ctx.fillStyle = 'red';
-//         ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
-//     }
-
-//     update() {
-//         this.draw();
-//         this.position.x += 1;
-
-//         // const waypoint = waypoints[0];
-//         // const yDistance = waypoint.y - this.position.y;
-//         // const xDistance = waypoint.x - this.position.x;
-//         // const angle = Math.atan2(yDistance, xDistance);
-//     }
-// }
-
-// // const enemy = new Enemy({position: { x: 200, y: 400} });
-// // const enemy2 = new Enemy({position: { x: 0, y: 400} });
-
-// function animate() {
-//     requestAnimationFrame(animate)
-//     ctx.drawImage(image, 0, 0);
-//     drawCharacter(characterX, characterY);
-// }
-
-// animate();
-
-// const canvas = document.getElementById('canvas2');
-// const ctx = canvas.getContext('2d');
-
-// const image = new Image()
-// image.onload = () => {
-//     animate();
-// }
-// image.src = 'citytileset.png';
-// character.src = '../images/mob/mob1.gif';
-
-// function animate() {
-//     requestAnimationFrame(animate)
-//     ctx.drawImage(image, 0, 0);
-//     ctx.drawImage(character, 0, 0);
-// }
-
-// animate();
-
-// const character = new Image();
-// character.onload = () => {
-//     drawCharacter(characterX, characterY);
-// };
-// character.src = '../images/mob/mob1.gif';
-
-// function drawCharacter(x, y) {
-//   ctx.drawImage(character, x, y);
-// }
-
-// let characterX = 20;
-// let characterY = 30;
-
-// function moveCharacter(event) {
-//   switch(event.keyCode) {
-//     case 37: // 왼쪽 화살표
-//       characterX -= 10;
-//       break;
-//     case 38: // 위쪽 화살표
-//       characterY -= 10;
-//       break;
-//     case 39: // 오른쪽 화살표
-//       characterX += 10;
-//       break;
-//     case 40: // 아래쪽 화살표
-//       characterY += 10;
-//       break;
-//   }
-
-//   ctx.clearRect(0, 0, canvas.width, canvas.height);
-//   drawCharacter(characterX, characterY);
-// }
-
-// document.addEventListener('keydown', moveCharacter);
-
-const canvas = document.getElementById('canvas2');
-const ctx = canvas.getContext('2d');
+// 플레이어 좌표(계속 바뀌는 값이라 따로 빼기)
+let playerX = canvas.width/2-32;
+let playerY = canvas.height/2;
 
 ctx.fillStyle = 'white';
 ctx.fillRect(0, 0, canvas.width, canvas.height)
 
-const citystage = new Image()
-//image.src = '../'
-citystage.onload = () => {
-    ctx.drawImage(citystage, 0, 0);
+// 이미지 로드
+function loadImage(){
+    backgroundImage = new Image();
+    backgroundImage.src='../images/background/citytileset.png';;
+    playerImage = new Image();
+    playerImage.src='../images/mob/mob1.gif';
 }
-citystage.src = 'citytileset.png';
 
-class Enemy {
-    constructor({ position = { x: 0, y: 0 } }) {
-        this.position = position;
-        this.width = 100;
-        this.height = 100;
+// 화면에 계속 배경과 플레이어 보이도록 설정
+function render(){
+    const bgX = -playerX + canvas.width / 2;
+    const bgY = -playerY + canvas.height / 2;
+    // 배경 이미지 그리기
+    ctx.drawImage(backgroundImage, bgX, bgY);
+    // 캐릭터 그리기
+    ctx.drawImage(playerImage, playerX, playerY);
+}
+
+let keysDown={} // 내가 누른 버튼의 값들을 객체에 저장
+function setuKeyboardListener(){
+    document.addEventListener('keydown', function(event){
+        keysDown[event.key] = true // 키보드 누르고 있을 때
+    });
+    document.addEventListener('keyup', function(event){
+        delete keysDown[event.key] // 키보드 누르지 않을 때 객체 안의 값들 삭제
+    })
+}
+
+function move(){
+    if('ArrowRight' in keysDown){
+        playerX += 2;
+    } // 오른쪽 버튼 눌림
+    if('ArrowLeft' in keysDown){
+        playerX -= 2;
+    } // 왼쪽 버튼 눌림
+    if('ArrowUp' in keysDown){
+        playerY -= 2;
+    } // 위쪽 버튼 눌림
+    if('ArrowDown' in keysDown){
+        playerY += 2;
+    } // 아래쪽 버튼 눌림
+
+    // 플레이어를 스테이지 안에서만 있게 하려면?(캔버스를 벗어나지 않게)
+    if(playerX <= 0){
+        playerX = 0
+    }
+    if(playerX >= canvas.width-32){
+        playerX = canvas.width-32
     }
 
-    draw() {
-        ctx.fillStyle = 'red';
-        ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+    if(playerY <= 0){
+        playerY = 0
     }
-
-    update() {
-        this.draw();
-        this.position.x += 1;
-
-        // const waypoint = waypoints[0];
-        // const yDistance = waypoint.y - this.position.y;
-        // const xDistance = waypoint.x - this.position.x;
-        // const angle = Math.atan2(yDistance, xDistance);
+    if(playerY >= canvas.height-32){
+        playerY = canvas.height-32
     }
 }
 
-const enemy = new Enemy({position: { x: 200, y: 400} });
-const enemy2 = new Enemy({position: { x: 0, y: 400} });
-
-function animate() {
-    requestAnimationFrame(animate)
-    ctx.drawImage(image, 0, 0);
-    // enemy.update();
-    // enemy2.update();
+function main(){
+    move(); // 움직이면서 바뀐 좌표값
+    render(); // 화면에 보여 주기
+    requestAnimationFrame(main);
 }
-
-const character = new Image();
-character.src = '../images/mob/mob1.gif';
-character.onload = () => {
-  drawCharacter(characterX, characterY);
-};
-
-function drawCharacter(x, y) {
-  ctx.drawImage(character, x, y);
-}
-
-let characterX = canvas.width / 2;
-let characterY = canvas.height / 2;
-
-function moveCharacter(event) {
-  switch(event.keyCode) {
-    case 37: // 왼쪽 화살표
-      characterX -= 10;
-      break;
-    case 38: // 위쪽 화살표
-      characterY -= 10;
-      break;
-    case 39: // 오른쪽 화살표
-      characterX += 10;
-      break;
-    case 40: // 아래쪽 화살표
-      characterY += 10;
-      break;
-  }
-
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  drawCharacter(characterX, characterY);
-}
-
-document.addEventListener('keydown', moveCharacter);
+loadImage();
+setuKeyboardListener();
+main();
