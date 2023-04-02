602770a60c677214d0388cee39b272305fc0b94e
92Lee92 <devindeed92@gmail.com>
2023-04-02 13:31:47 +0900

    merge commit

--- menu/game.js
+++ menu/game.js
@@ -10,27 +10,6 @@ const ctx = canvas.getContext('2d');
 canvas.width = 1024;
 canvas.height = 576;
 
-
-// ctx.fillStyle = 'white';
-// ctx.fillRect(0, 0, canvas.width, canvas.height)
-
-//스테이지1 불러오기
-const citystage = new Image()
-citystage.onload = () => {
-    ctx.drawImage(citystage, 0, 0);
-}
-citystage.src = '../images/background/citytile1.png';
-
-function animate() {
-    requestAnimationFrame(animate)
-    // clearRect(0, 0, canvas.width, canvas.height)
-}
-
-//타이머
-let gameInterval;
-
-
-
 //캐릭터 선택
 const ch1 = document.getElementById('ch1');
 const ch2 = document.getElementById('ch2');
@@ -46,56 +25,96 @@ let selectedCharacter = ch1;
 // selectedCharacter.height=50;
 
 
-//게임시작
-const characterSeletion = document.getElementById('characterSelection');
-function startGame() {
-
-    //캐릭터 선택 화면 숨기기
-    characterSeletion.classList.add("hidden"); 
+// Player
+class Player{
+    playerX
+    playerY
+    playerImage
+    health
     
+    constructor(){
+        this.playerImage = selectedCharacter
+        this.playerX = canvas.width/2;
+        this.playerY = canvas.height/2;
+        this.health = 100;
+    }
+    // 플레이어가 몬스터를 공격하는 함수
+}
 
-     //움직이는 코드 가져옴
-   setuKeyboardListener();
- main();
+// Monster
+class Monster {
+    x
+    y
+    size
+    speed
+    direction
+    image
+    health
+    
+    constructor() {
+        // 몬스터의 위치 랜덤으로 생성하기
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+        // 몬스터의 크기 랜덤으로 생성하기
+        this.size = Math.random() * 50 + 20;
+        // 몬스터 이동 속도 랜덤으로 생성하기
+        this.speed = Math.random() * 2 + 1;
+        // 몬스터 이동 방향 랜덤으로 생성하기
+        // const direction = Math.random() * 2 * Math.PI;
+        this.direction = Math.atan2(player.playerY - this.y, player.playerX - this.x);
+        // 몬스터 정보를 객체로 저장하고 배열에 추가하기
+        this.image = monsterImage;
+        this.health = 100;
+    }
+}
+
+// 몬스터 이미지 객체 생성
+const monsterImage = new Image();
+monsterImage.src = '../images/mob/mob2.gif';
+
+// 몬스터 정보를 저장하는 배열
+let monsters = [];
+
+// 몬스터 그리는 함수
+function drawMonsters() {
+  for (let i = 0; i < monsters.length-i; i++) {
+    const monster = monsters[i];
+    ctx.drawImage(monster.image, monster.x, monster.y, monster.size, monster.size);
+  }
+}
+
+// 새로운 몬스터 생성 함수
+function createMonster() {
+    if(monsters.length <= 50) {
+        const monster = new Monster();
+        monsters.push(monster);
+    }
+  
+    // 모든 몬스터 그리기
+    drawMonsters();
   }
 
-// class Player{
-//   //position이랑 velocity
-//   playerX
-//   playerY
-//   playerImage
+// 몬스터 움직임 구현
+function moveMonsters() {
+  for (let i = 0; i < monsters.length; i++) {
+    const monster = monsters[i];
 
-//   constructor(){
-//   this.playerImage = selectedCharacter
-//   this.playerX = canvas.width/2;
-//   this.playerY = canvas.height/2;
-//   }
-// }
+    // 몬스터의 위치 변경
+    monster.x += monster.speed * Math.cos(monster.direction);
+    monster.y += monster.speed * Math.sin(monster.direction);
 
-const player = new Player(32,56,selectedCharacter,{right:selectedCharacter,
-  left:selectedCharacter,
-  touchedLeft: selectedCharacter,
-  touchedRight: selectedCharacter})
-// player.playerImage=selectedCharacter;
-
-function render(){
-  const bgX = -player.position.x;
-  // console.log(bgX);
-  const bgY = -player.position.y;
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.drawImage(citystage, bgX, bgY, 2300, 1200);
-  ctx.drawImage(selectedCharacter, player.position.x, player.position.y);
+    // 화면 밖으로 나갔을 경우 위치 초기화
+    if (monster.x < -monster.size || monster.x > canvas.width + monster.size ||
+      monster.y < -monster.size || monster.y > canvas.height + monster.size) {
+      monster.x = Math.random() * canvas.width;
+      monster.y = Math.random() * canvas.height;
+    }
+    monster.direction = Math.atan2(player.playerY - monster.y, player.playerX - monster.x);
+  }
 }
 
-let keysDown={} // 내가 누른 버튼의 값들을 객체에 저장
-function setuKeyboardListener(){
-  document.addEventListener('keydown', function(event){
-      keysDown[event.key] = true // 키보드 누르고 있을 때
-  });
-  document.addEventListener('keyup', function(event){
-      delete keysDown[event.key] // 키보드 누르지 않을 때 객체 안의 값들 삭제
-  })
-}
+// 일정 주기로 몬스터 움직이기
+setInterval(moveMonsters, 100);
 
 function move(){
   if('ArrowRight' in keysDown){
@@ -128,12 +147,23 @@ function move(){
   }
 }
 
-function main(){
-  move(); // 움직이면서 바뀐 좌표값
-  render(); // 화면에 보여 주기
-  createMonster();
-  requestAnimationFrame(main);
+//스테이지1 불러오기
+const citystage = new Image();
+citystage.src = '../images/background/citytile1.png';
+
+citystage.onload = () => {
+    ctx.drawImage(citystage, 0, 0);
 }
+
+function render(){
+  const bgX = -player.playerX;
+  const bgY = -player.playerY;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.drawImage(citystage, bgX, bgY, 2300, 1200);
+  ctx.drawImage(selectedCharacter, player.playerX, player.playerY);
+}
+
+const player = new Player()
 
 ch1.addEventListener('click', function() {
   // 캐릭터1을 선택했을 때
@@ -170,81 +200,26 @@ ch5.addEventListener('click', function() {
     startButton.style.display = 'block';
   });  
 
-// 몬스터 이미지 객체 생성
-const monsterImage = new Image();
-monsterImage.src = '../images/mob/mob2.gif';
+//게임시작
+const characterSeletion = document.getElementById('characterSelection');
 
-// 몬스터 정보를 저장하는 배열
-let monsters = [];
-
-// 몬스터 그리는 함수
-function drawMonsters() {
-  for (let i = 0; i < monsters.length-i; i++) {
-    const monster = monsters[i];
-    ctx.drawImage(monster.image, monster.x, monster.y, monster.size, monster.size);
-  }
+function startGame() {
+    //캐릭터 선택 화면 숨기기
+    characterSeletion.classList.add("hidden"); 
+    
+    //움직이는 코드 가져옴
+    setuKeyboardListener();
 }
 
-// 새로운 몬스터 생성 함수
-function createMonster() {
-
-  // 몬스터의 위치 랜덤으로 생성하기
-  const x = Math.random() * canvas.width;
-  const y = Math.random() * canvas.height;
-
-  // 몬스터의 크기 랜덤으로 생성하기
-  const size = Math.random() * 50 + 20;
-
-  // 몬스터 이동 속도 랜덤으로 생성하기
-  const speed = Math.random() * 2 + 1;
-
-  // 몬스터 이동 방향 랜덤으로 생성하기
-  // const direction = Math.random() * 2 * Math.PI;
-  const direction = Math.atan2(player.playerY - y, player.playerX - x);
-
-  // 몬스터 정보를 객체로 저장하고 배열에 추가하기
- 
-  if(monsters.length <= 50) {
-  monsters.push({
-    x: x,
-    y: y,
-    size: size,
-    speed: speed,
-    direction: direction,
-    image: monsterImage
+let keysDown={} // 내가 누른 버튼의 값들을 객체에 저장
+function setuKeyboardListener(){
+  document.addEventListener('keydown', function(event){
+      keysDown[event.key] = true // 키보드 누르고 있을 때
   });
-};
-
-  // 모든 몬스터 그리기
-  drawMonsters();
+  document.addEventListener('keyup', function(event){
+      delete keysDown[event.key] // 키보드 누르지 않을 때 객체 안의 값들 삭제
+  })
 }
-
-// 일정 주기로 몬스터 생성하기
-// if(monsters.length <= 10) {
-// setInterval(createMonster, 3000);
-// }
-
-// 몬스터 움직임 구현
-function moveMonsters() {
-  for (let i = 0; i < monsters.length; i++) {
-    const monster = monsters[i];
-
-    // 몬스터의 위치 변경
-    monster.x += monster.speed * Math.cos(monster.direction);
-    monster.y += monster.speed * Math.sin(monster.direction);
-
-    // 화면 밖으로 나갔을 경우 위치 초기화
-    if (monster.x < -monster.size || monster.x > canvas.width + monster.size ||
-      monster.y < -monster.size || monster.y > canvas.height + monster.size) {
-      monster.x = Math.random() * canvas.width;
-      monster.y = Math.random() * canvas.height;
-    }
-    monster.direction = Math.atan2(player.playerY - monster.y, player.playerX - monster.x);
-  }
-}
-
-// 일정 주기로 몬스터 움직이기
-setInterval(moveMonsters, 100);
 
 startButton.addEventListener('click', function() {
   // 게임 시작 버튼을 눌렀을 때
@@ -255,56 +230,16 @@ startButton.addEventListener('click', function() {
   if(monsters.length <= 10) {
     setInterval(createMonster, 10000);
     }
-  
-  // 선택한 캐릭터 그리기
-  //ctx.drawImage(selectedCharacter, 500, 500);
-  
   // 게임 루프 실행
   gameLoop();
 });
 
+//gameLoop
 function gameLoop() {
-  // 게임 루프
-  // 게임의 상태 업데이트
-
+  move(); // 움직이면서 바뀐 좌표값
+  render(); // 화면에 보여 주기
   createMonster();
-  // 게임 화면 그리기
-  //ctx.clearRect(0, 0, canvas.width, canvas.height);
-   //타이머 시작하기
-   clearInterval(gameInterval);
-
-   const startTime = Date.now();
-   const endTime = startTime + (3 * 60 * 1000)+2000;
-
-   let timerInterval = setInterval(() => {
-     const remainingTime = endTime - Date.now();
-
-     if (remainingTime <= 0) {
-       clearInterval(timerInterval);
-       alert("Time's up!");
-     } else {
-       const seconds = Math.floor(remainingTime / 1000) % 60;
-       const minutes = Math.floor(remainingTime / 1000 / 60);
-
-
-       ctx.clearRect(5, 10, 200, 50);
-       //ctx.drawImage(citystage, 0, 0);
-       ctx.font = "30px Arial";
-       ctx.fillText(`Time ${minutes}:${seconds < 10 ? "0" : ""}${seconds}`, 10, 50);
-     }
-   }, 1000);
-  
-   //타이머를 멈추는 function
-   function stopTimer() {
-     clearInterval(timerInterval);
-     alert("Timer stopped.");
-
-  // 캐릭터 등 게임 요소 그리기
-  ctx.drawImage(selectedCharacter, 500, 500);
 
   // 다음 프레임에 대한 처리를 위해 루프 재귀 호출
   requestAnimationFrame(gameLoop);
-}
-
-
 }
